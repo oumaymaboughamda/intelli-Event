484d2e7f05de9731de92eaa3b36b51a11ddbcfa1
neda <nedakadri322@gmail.com>
2026-03-19 13:55:26 +1200

    fix: adding all files inside Event

---
+++ Event/src/app/registre/registre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registre',
+  imports: [],
+  templateUrl: './registre.component.html',
+  styleUrl: './registre.component.css'
+})
+export class RegistreComponent {
+
+}
